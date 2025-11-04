d727134db756f2ca2588951e8416d04a8620cf20
Rianda Rafqi <riandarafqi321@gmail.com>
2025-11-04 21:42:25 +0700

    first commmit

---
+++ src/main/java/id/ac/unpas/pp2_c_233040092/PP2_C_233040092.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.pp2_c_233040092;
+
+/**
+ *
+ * @author Rianda Rafqi
+ */
+public class PP2_C_233040092 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
